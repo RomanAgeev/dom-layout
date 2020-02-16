0a4997cb808d858f0c864c9332eb493105ceb70b
Roman Ageev <roman.ageyev@devexpress.com>
2020-02-16 14:15:32 +0300

    layout class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { visualizeLayout } from "./visualizer";
-import { LayoutItem, LayoutDirection } from "./layout";
+import { LayoutItem, LayoutDirection, Layout } from "./layout";
 
 window.onload = function() {
     const container = document.querySelector("#container") as HTMLElement;
 
-    const layout = new LayoutItem(LayoutDirection.Horizontal, "");
+    const root = new LayoutItem(LayoutDirection.Horizontal, "");
     const item1 = new LayoutItem(LayoutDirection.Vertical, "lightgreen", 1);
     const item2 = new LayoutItem(LayoutDirection.Vertical, "", 2);
     const item3 = new LayoutItem(LayoutDirection.Vertical, "blue", 3);
@@ -16,7 +15,7 @@ window.onload = function() {
     const item23 = new LayoutItem(LayoutDirection.Horizontal, "", 2);
     item23.children.push(new LayoutItem(LayoutDirection.Vertical, "gray", 1));
     item23.children.push(new LayoutItem(LayoutDirection.Vertical, "lightgray", 2));
-    item23.children.push(new LayoutItem(LayoutDirection.Vertical, "darkgray", 3));    
+    item23.children.push(new LayoutItem(LayoutDirection.Vertical, "darkgray", 3));
 
     item2.children.push(new LayoutItem(LayoutDirection.Horizontal, "red", 2));
     item2.children.push(item22);
@@ -25,9 +24,10 @@ window.onload = function() {
     item3.children.push(new LayoutItem(LayoutDirection.Horizontal, "blue"));
     item3.children.push(new LayoutItem(LayoutDirection.Horizontal, "lightblue"));
 
-    layout.children.push(item1);
-    layout.children.push(item2);
-    layout.children.push(item3);
+    root.children.push(item1);
+    root.children.push(item2);
+    root.children.push(item3);
 
-    visualizeLayout(layout, container);
+    const layout = new Layout(container, root);
+    layout.build();
 };
--- src/layout.ts
+++ src/layout.ts
@@ -1,3 +1,5 @@
+import { identity, constant } from "./utils";
+
 export enum LayoutDirection {
     Horizontal,
     Vertical
@@ -11,4 +13,88 @@ export class LayoutItem {
     }
 
     readonly children: LayoutItem[] = [];
+}
+
+export class Layout {
+    constructor(
+        readonly container: HTMLElement,
+        readonly root: LayoutItem) {
+
+        this._itemOver = this._itemOver.bind(this);
+        this._itemOut = this._itemOut.bind(this);
+        this._itemClick = this._itemClick.bind(this);
+
+        this.container.addEventListener("mouseover", this._itemOver);
+        this.container.addEventListener("mouseout", this._itemOut);
+        this.container.addEventListener("click", this._itemClick);
+    }
+
+    private readonly _domToLayout = new Map<HTMLElement, LayoutItem>();
+
+    build(): void {
+        this._buildItem(this.root, this.container);
+    }
+
+    private _buildItem(item: LayoutItem, placeholder: HTMLElement): void {
+        if (!item.children || item.children.length === 0) {
+            placeholder.style.background = item.color;
+            return;
+        }
+    
+        const sum = item.children.reduce((acc, child) => acc + child.weight, 0);
+    
+        let getLeft: (start: number) => number;
+        let getTop: (start: number) => number;
+        let getWidth: (size: number) => number;
+        let getHeight: (size: number) => number;
+        if (item.direction === LayoutDirection.Horizontal) {
+            getLeft = identity;
+            getTop = constant(0);
+            getWidth = identity;
+            getHeight = constant(100);
+        } else {
+            getLeft = constant(0);
+            getTop = identity;
+            getWidth = constant(100);
+            getHeight = identity;
+        }
+    
+        let start = 0;
+        for (const child of item.children) {
+            const size = child.weight / sum * 100;
+    
+            const element = document.createElement("div");
+            element.style.position = "absolute";
+            element.style.left = getLeft(start) + "%";
+            element.style.top = getTop(start) + "%";
+            element.style.width = getWidth(size) + "%";
+            element.style.height = getHeight(size) + "%";
+            placeholder.appendChild(element);
+
+            this._domToLayout.set(element, child);
+    
+            start += size;
+    
+            this._buildItem(child, element);
+        }
+    }
+
+    private _itemOver(e: Event): void {
+        (e.target as HTMLElement).style.border = "5px solid black"
+        e.preventDefault();
+    }
+
+    private _itemOut(e: Event): void {
+        (e.target as HTMLElement).style.border = "";
+        e.preventDefault();
+    }
+
+    private _itemClick(e: Event): void {
+        const item = this._domToLayout.get(e.target as HTMLElement);
+        if (!item) {
+            return;
+        }
+
+        alert(item.color);
+    }
 }
